09c99f2cd15ecf1aa511f243ff50614e42e3a7d8
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-01-04 22:25:22 +0700

    feat: add logout route

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -124,3 +124,10 @@ export const loginUser = asyncHandler(async (req, res) => {
         })
     }
 })
+
+export const logoutUser = asyncHandler(async (req, res) => {
+    res.clearCookie('token')
+    res.status(200).json({
+        message: 'Logged out'
+    })
+})
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import {
   registerUser,
-  loginUser
+  loginUser,
+  logoutUser
 } from '../controllers/auth/userController.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
+router.get('/logout', logoutUser)
 
 export default router
